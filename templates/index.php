0406708c4a7ebe7232867f8c4378d5a9be2ea2f4
Felippe <felippe228@gmail.com>
2019-10-16 15:55:14 -0300

    template

---
+++ templates/index.php
@@ -0,0 +1,6 @@
+<?php include "header.php"; ?>
+
+    <h1>Pagina de produtos</h1>
+    <p><a href="produtos.php">Ir para a Pagina Inicial</p>
+
+<?php include "footer.php"; ?>
---
+++ templates/produtos.php
@@ -0,0 +1,6 @@
+<?php include "header.php"; ?>
+
+    <h1>Pagina Inicial</h1>
+    <p><a href="index.php">Voltar a Pagina de Produtos</p>
+
+<?php include "footer.php"; ?>
